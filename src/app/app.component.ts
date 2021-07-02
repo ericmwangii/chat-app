1d7594187b7b00bb1777a990d7ec30cb4bdb5584
ericmwangii <ericmwangii101@gmail.com>
2021-07-02 13:31:02 +0300

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ChatService } from './Services/chat.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'e-commerceapp';
+  title = 'Chat App';
 
   constructor(public chatService: ChatService) {}
 }
